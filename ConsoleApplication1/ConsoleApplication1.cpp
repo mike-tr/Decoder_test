a2bb8952cfe1f130d8b45e645b530b607aab1db5
Mike <Miket9717@gmail.com>
2020-01-02 23:05:50 +0200

    v0.15 - Created class, created some basic unfinished methods, partly working

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,53 +1,76 @@
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
 #include <math.h>
 
 using namespace std;
 
-long pa(int m, int e, int n);
 long mp(long m, long q, long p);
+long compressMod(int num, int power, int base);
+long modPow(long num, int power, long base);
+long encode(int data_bit, int key, int base);
 int main()
 {
-	int p = 307;
-	int q = 311;
+	int p = 29; // 11
+	int q = 23; // 7;
+	int e = 5; //7; works!
 
 	long n = p * q;
 	long fi = (p - 1)*(q - 1);
-	long e = fi - 1;
 
+	int d = mp(e, q, p);
 
+	//cout << "test : " << mp(11, 3, 5) << endl;
+	cout << " d : " <<d << " , e :" << e << " , fi :" << fi << endl;
 
-	int t = pa(3, (7 * 5) - 1, 7 * 5);
-
-	cout << mp(4, 3, 5);
-	//cout << t << endl;
+	int m = 31;
+	int dcoded = encode(m, e, n);
+	d = 493;
+	cout << dcoded << " message : " << m << endl;
+	cout << "try e? " << encode(dcoded, e, n) << endl;
+	cout << "try d? " << encode(dcoded, d, n) << endl;
 	//cout << pa(t, (7*5)-1, 7*5) << endl;
 }
 
-long pa(int m, int e, int n) {
-	return (long)pow(m, e) % n;
+long encode(int data_bit, int key, int base) {
+	return compressMod(data_bit, key, base);
 }
 
 long mp(long m, long q, long p) {
 	long n = q * p;
+	cout << n << endl;
 	if (p == 1)
 		p = 2;
 	if (q == 1)
 		q = 2;
-	unsigned long long int x = pow(m, (q - 1)*(p - 1) - 1);
-	return x % n;
+	
+	long base = (q - 1) * (p - 1) - 1;
+	cout << base << " b " << q * p << endl;
+	return compressMod(m, base, base + 1);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+long compressMod(int num,int power, int base) {
+	//cout << num << " ," << power << " ," << base << endl;
+	if (power > 2) {
+		// Get a smaller number!
+		int tempv = power % 2;
+		int mval = modPow(num, tempv, base);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+		int cpow = power - tempv;
+		int sp = cpow / 2;
+
+		long cer = compressMod(num, sp, base);
+		long x = compressMod(cer, 2, base);
+
+		return (x * mval) % base;
+	}
+	return modPow(num, power, base);
+}
+
+long modPow(long num, int power, long base) {
+	unsigned long long vnum = pow(num, power);
+	return vnum % base;
+}
+
+
